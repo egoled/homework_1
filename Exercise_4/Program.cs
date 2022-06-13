951470255bda6dbfebd0025de4fb101718d87cb1
«Egor <egoled8@gmail.com>
2022-06-13 15:35:12 +0300

    Задача 4 от 6го июня

---
+++ Exercise_4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Введите число ");
+string NumStr = Console.ReadLine ();
+int N = int.Parse(NumStr);
+int A = 1;
+while (A <= N)
+{
+    if (A % 2 == 0)
+    Console.WriteLine (A);
+    A = A+1;
+};
+
