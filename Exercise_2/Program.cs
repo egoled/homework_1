ffcafc2c0cb6cdad7c52138126f07e80d68dcc8c
«Egor <egoled8@gmail.com>
2022-06-13 15:34:25 +0300

    Задача 2  от 6го июня

---
+++ Exercise_2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
+Console.WriteLine("Введите первое число > ");
+string StringOne = Console.ReadLine ();
+int One = int.Parse (StringOne);
+
+Console.WriteLine("Введите второе число > ");
+string StringTwo = Console.ReadLine ();
+int Two = int.Parse (StringTwo);
+
+Console.WriteLine("Введите третье число > ");
+string StringThree = Console.ReadLine ();
+int Three = int.Parse (StringThree);
+int max = One;
+if (max == Two && max == Three &&  Two == Three)
+{
+    Console.WriteLine("Числа должны быть разными =)");  
+};
+if (max < Two)
+{
+    max = Two;
+};
+if (max < Three)
+{
+    max = Three;
+};
+Console.WriteLine ("Самое большое число " +max);
